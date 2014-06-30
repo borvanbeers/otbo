8f436ad4d4d794a399e82a0b2ad241ecebe2f022
A1rPun <thombakker@gmail.com>
2014-06-30 19:45:14 +0200

    Refactor makeImages

--- js/imageLoader.js
+++ js/imageLoader.js
@@ -40,5 +40,35 @@ function makeImages()
     }
 }
 
+(function(){
+	var wolfImages = ["CrazyWolf.jpg", "CrazyWolf.jpg", "CrazyWolf.jpg", "CrazyWolf.jpg"];
+
+	makeImages(wolfImages,function(img){
+		console.log(img.CrazyWolf.src);
+	});
+
+	function makeImages(images, callback)
+	{
+		var result = {};
+		var loadCount = 0;
+		var imagesToLoad = images.length;
+		
+		for(var i = imagesToLoad; i--;)
+		{
+			var name = source.split(".")[0];
+			console.log(name);
+			var img = new Image();			
+			img.onload = function () {
+				if (++loadCount >= imagesToLoad) {
+					callback(result);
+				}
+			};
+			img.src = images[i];
+			result[name] = img;
+		}
+		console.log("Loop ended");
+	}
+}());
+
 
 
