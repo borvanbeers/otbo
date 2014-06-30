26f7f3bfb76428f45fa5f02009090dadf79e770b
A1rPun <thombakker@gmail.com>
2014-06-30 22:44:50 +0200

    First concept
    - Gameloop is looping (duh!)
    - Can draw lines with mousedown+mousmove

--- js/main.js
+++ js/main.js
@@ -3,12 +3,35 @@
  */
 (function(global){
 	/*
+	 * Constants
+	 */
+	var LINE_LENGTH = 100;
+	/*
 	 * Global variables
 	 */
 	var canvas = document.getElementById('otbo'),
-		ctx = canvas.getContext('2d');
+		ctx = canvas.getContext('2d'),
+		mouseIsDown = false,
+		mouseStart,
+		mouseCurrent,
+		players = [];
 
-	
+	//Set width/height to 100&
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	//Detect mouseIsdown
+	canvas.addEventListener('mousedown', function(e){		
+		mouseIsDown = detectClick(e);
+	});
+	canvas.addEventListener('mouseup', function(e){
+		if(mouseIsDown){
+			plotMouseForce();
+			mouseIsDown = false;
+		}		
+	});
+	canvas.addEventListener('mousemove', function(e){
+		mouseCurrent = getMousePos(canvas, e);
+	});
 	/*
 	 * Helper methods
 	 */
@@ -42,7 +65,13 @@
 	function circlevscircle(circle1, circle2) {
         return (Math.sqrt(Math.pow(circle1.position.x - circle2.position.x, 2) + Math.pow(circle1.position.y - circle2.position.y, 2))) < circle1.radius + circle2.radius;
     }
-
+	//clamp a value to a minimum or maximum
+	function clamp(n,min,max) {
+        if (n < min) return min;
+        if (n > max) return max;
+        return n;
+    }
+	
 	
 	/*
 	 * Classes
@@ -68,6 +97,62 @@
 	
 	
 	/*
+	 * Main functions
+	 */
+	function detectClick(e){
+		var click = getMousePos(canvas,e);
+			clickObject = new Olo(new Vector(click.x,click.y),1);
+			
+		for(var i = players.length; i--;){
+			var player = players[i];
+			
+			if(circlevscircle(player,clickObject)){
+				mouseStart = player;
+				return true;
+			};
+		}
+		return false;
+	}
+	
+	function plotMouseForce(){	
+		var force = 50;//lower is faster
+		mouseStart.xSpeed = (mouseCurrent.x - mouseStart.position.x) / force;
+		mouseStart.ySpeed = (mouseCurrent.y - mouseStart.position.y) / force;		
+		mouseStart = null;
+	}
+	
+	function plotObjects(){
+		for(var i = players.length; i--;){
+			var player = players[i];
+			
+			player.position.x += player.xSpeed;
+			player.position.y += player.ySpeed;
+			//if (out of bounds) delete;
+		}
+	}
+	
+	function drawObjects(){
+		
+		//Draw players
+		for(var i = players.length; i--;){
+			var player = players[i];
+			ctx.beginPath();
+			ctx.arc(player.position.x, player.position.y, player.radius, 0, 2 * Math.PI, false);
+			ctx.fillStyle = player.color;
+			ctx.fill();
+		}
+		
+		
+		//Draw mouse line
+		if(mouseIsDown){
+			ctx.beginPath();
+			ctx.moveTo(mouseStart.position.x, mouseStart.position.y);
+			ctx.lineTo(mouseCurrent.x, mouseCurrent.y);
+			ctx.stroke();
+		}
+	}
+	
+	/*
 	 * Gameloop
 	 */
 	function loop() {
@@ -80,10 +165,10 @@
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
 	}
 	function update() {
-		//plotObjects();
+		plotObjects();
 	}
 	function draw() {
-		//drawObjects();
+		drawObjects();
 	}
 	function queue() {
 		window.requestAnimationFrame(loop);
@@ -98,8 +183,20 @@
         };
         loadImages(sources, function (i) {
 			ctx.drawImage(i.wolf, 0, 0);
+			
+			for(var i = 20; i--;){			
+				var player = new Olo(
+					new Vector(
+						~~(Math.random() * canvas.width) + 1,
+						~~(Math.random() * canvas.height) + 1
+					),
+					~~(Math.random() * 20) + 10
+				);
+				player.color = '#'+Math.floor(Math.random()*16777215).toString(16);
+				players.push(player);
+			}
+			loop();
         });
-		//loop();
 	}
 	init();
 }(window));
